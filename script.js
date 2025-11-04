9d99ceff1eced7006ffaeaa2713830f40b94e6ff
Ved Pawar <vedpawar1410@gmail.com>
2025-11-04 10:45:21 +0530

    adds book only if it doesnt exist

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function Book(title, author, pages, bookRead){
+function Book(id, title, author, pages, bookRead){
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -12,12 +13,20 @@ function Book(title, author, pages, bookRead){
 let myLibrary = [];
 function addBookToLibrary(title, author, pages, bookRead) {
     // take params, create a book then store it in the array
-    const newBook = new Book(title, author, pages, bookRead);
+    let id = crypto.randomUUID();
+
+    // Check if book with same id OR same title+author exists
+    const exists = myLibrary.some(book =>
+        book.id === id ||
+        (book.title.toLowerCase() === title.toLowerCase() && book.author.toLowerCase() === author.toLowerCase())
+    );
+    if (exists) {
+        console.log("This book already exists in the library!");
+        return;
+    }
+
+    const newBook = new Book(id, title, author, pages, bookRead);
     myLibrary.push(newBook);
     console.log(myLibrary);
-
-
 }
-
-addBookToLibrary('AI 2041','Chen Qiufan', 400, true);
 
