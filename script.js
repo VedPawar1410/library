bb4fe307f5bc8441859974e608c1772d1bc2adab
Ved Pawar <vedpawar1410@gmail.com>
2025-11-04 10:09:11 +0530

    bookRead property set to false

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function Book(title, author, pages, bookRead){
     this.title = 'Harry Potter and the Philosopher\'s Stone';
     this.author = 'J. K. Rowling';
     this.pages = 493;
-    this.bookRead = 'Not read yet';
+    this.bookRead = false;
 
     this.info = function(){
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.bookRead}`
@@ -11,8 +11,5 @@ function Book(title, author, pages, bookRead){
 const myBook = new Book();
 console.log(myBook.info());
 
-//every object contains a prototype
-if(Object.getPrototypeOf(myBook) === Book.prototype){
-    console.log("Yes");
-} // returns true
+
 
